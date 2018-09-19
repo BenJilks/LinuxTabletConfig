e7ad3131c0d4a78c0f7c1d79af3ea2336ee719dc
BenJilks <benjyjilks@gmail.com>
2018-09-19 14:43:16 +0100

    GUI

--- Src/LinuxTabletConfig.cpp
+++ Src/LinuxTabletConfig.cpp
@@ -5,32 +5,8 @@
 #include <X11/extensions/Xrandr.h>
 #include <X11/extensions/Xinerama.h>
 #include <X11/XKBlib.h>
-
-/*
 #include <gtk/gtk.h>
-static void activate (GtkApplication* app, gpointer user_data)
-{
-	GtkWidget *window;
 
-	window = gtk_application_window_new (app);
-	gtk_window_set_title (GTK_WINDOW (window), "Window");
-	gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-	gtk_widget_show_all (window);
-}
-
-int main (int argc, char **argv)
-{
-	GtkApplication *app;
-	int status;
-
-	app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-	g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-	status = g_application_run (G_APPLICATION (app), argc, argv);
-	g_object_unref (app);
-
-	return status;
-}
-*/
 
 void scan_wacom()
 {
@@ -57,8 +33,25 @@ void scan_wacom()
 	XFreeDeviceList(info);
 }
 
-int main()
+static void activate(GtkApplication* app, gpointer user_data)
 {
-	scan_wacom();
-	return 0;
+	GtkWidget *window;
+
+	window = gtk_application_window_new (app);
+	gtk_window_set_title (GTK_WINDOW (window), "Window");
+	gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+	gtk_widget_show_all (window);
+}
+
+int main (int argc, char **argv)
+{
+	GtkApplication *app;
+	int status;
+
+	app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+	g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+	status = g_application_run (G_APPLICATION (app), argc, argv);
+	g_object_unref (app);
+
+	return status;
 }
