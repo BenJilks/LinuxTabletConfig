5e20cc12395c8220af5b3d4e9802d770b6e1ffec
BenJilks <benjyjilks@gmail.com>
2022-08-03 17:54:42 +0100

    Port `main` to use a Qt application.

--- src/LinuxTabletConfig.cpp
+++ src/LinuxTabletConfig.cpp
@@ -1,27 +1,30 @@
+#include <string>
 #include <iostream>
 #include <pwd.h>
+#include <unistd.h> 
 #include <sys/dir.h> 
-#include <gtkmm/application.h>
+#include <sys/stat.h> 
+
+#include <QApplication>
 #include "Config.hpp"
 #include "Window.hpp"
 #include "DeviceManager.hpp"
-#include "Mapper.hpp"
-using std::string;
 
 int main(int argc, char **argv)
 {
-	struct passwd *pw = getpwuid(getuid());
-	mkdir((string(pw->pw_dir) + "/.local").c_str(), 0777);
-	mkdir((string(pw->pw_dir) + "/.local/share").c_str(), 0777);
-	mkdir((string(pw->pw_dir) + "/.local/share/LinuxTabletConfig").c_str(), 0777);
+    QApplication app(argc, argv);
 
-	string data_path = string(pw->pw_dir) + "/.local/share/LinuxTabletConfig";
+	struct passwd *pw = getpwuid(getuid());
+	mkdir((std::string(pw->pw_dir) + "/.local").c_str(), 0777);
+	mkdir((std::string(pw->pw_dir) + "/.local/share").c_str(), 0777);
+	mkdir((std::string(pw->pw_dir) + "/.local/share/LinuxTabletConfig").c_str(), 0777);
+
+	string data_path = std::string(pw->pw_dir) + "/.local/share/LinuxTabletConfig";
 	Config config(data_path + "/config.json");
-	auto app = Gtk::Application::create(
-		argc, argv, "org.benjilks.ltc");
 
 	DeviceManager dm;
 	LTCWindow window(dm, config);
-
-	return app->run(window);
+    window.show();
+	return app.exec();
 }
+
